a0c071fbaa108c61d7e30fc151dc30c14a96f0ed
Auston Stewart <auston@shedbuilt.net>
2018-05-31 11:08:30 -1000

    Updating to v7.60.0 for blank

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Configure
 ./configure --prefix=/usr                           \
             --disable-static                        \
             --enable-threaded-resolver              \
-            --with-ca-path=/etc/ssl/certs
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
-rm -rf docs/examples/.deps
-find docs \( -name Makefile\* \
-          -o -name \*.1       \
-          -o -name \*.3 \)    \
-          -exec rm {} \;
-install -v -d -m755 ${SHED_FAKE_ROOT}/usr/share/doc/curl-7.55.1
-cp -v -R docs/* ${SHED_FAKE_ROOT}/usr/share/doc/curl-7.55.1
+            --with-ca-path=/etc/ssl/certs &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+rm -rf docs/examples/.deps || exit 1
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    find docs \( -name Makefile\* \
+              -o -name \*.1       \
+              -o -name \*.3 \)    \
+              -exec rm {} \; &&
+    install -vdm755 "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}" &&
+    cp -v -R docs/* "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+fi
